ae13314181642bd30ec799cf958b3fb4788dcc39
imthebest1997 <imthebest_1997@hotmail.com>
2023-07-14 20:07:48 -0500

    Correcion de bug en gravedad

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,13 +17,12 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 moveDirection;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        print(Physics.gravity.y);
+        print(moveDirection.y);
     }
 
-    // Update is called once per frame
     void Update()
     {
         float y = moveDirection.y;
@@ -36,15 +35,17 @@ public class PlayerController : MonoBehaviour
         moveDirection *= moveSpeed;
         moveDirection.y = y;
 
-        if (characterController.isGrounded && z < 0)
-        {
-            z = -2f;
-        }
 
+        //TODO: NEW
+        if(characterController.isGrounded && moveDirection.y< 0)
+        {
+            moveDirection.y -= (2 * gravityScale);
+        }
 
         //Salto
         if (characterController.isGrounded)
         {
+//            print(moveDirection.y);
 //            moveDirection.y = 0f;
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -53,19 +54,19 @@ public class PlayerController : MonoBehaviour
         }
 
         //Agregar gravedad  
-        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;// * gravityScale
 
         characterController.Move(moveDirection * Time.deltaTime);
 
         //Rotar al jugador cuando hay movimiento
-        if (x != 0 ||  z != 0)
+        if (x != 0 || z != 0)
         {
             transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
             Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
-            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation,rotateSpeed * Time.deltaTime);            
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
         }
 
-        animator.SetFloat("Speed",Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
+        animator.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
         animator.SetBool("Grounded", characterController.isGrounded);
     }
 }
